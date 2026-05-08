a662834e78b0d6e463a39f94cedd26073c836d1d
Kyle <kjjust7@gmail.com>
2026-05-08 00:57:56 -0700

    Adjust navbar fopr mobile clients

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,16 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0 w-full flex items-center justify-around py-5 px-24 border-b border-grey-700 bg-black z-50'>
-        <Link href="/" className="transition duration-300 hover:scale-110">Home</Link>
-        <ul className="flex gap-10 text-lg">
-            <Link href="/About" className="text-grey-300 hover:text-white trasition-colors">About Us</Link>
-            <Link href="/2025" className="text-grey-300 hover:text-white trasition-colors">2025 Board</Link>
-        </ul>
-    </nav>
-  )
-}
+    <nav className="fixed top-0 w-full flex items-center justify-between py-4 px-6 md:px-24 border-b border-gray-700 bg-black z-50">
+      
+      <Link href="/" className="transition duration-300 hover:scale-110 text-sm md:text-base">
+        Home
+      </Link>
 
-export default Navbar
+      <ul className="flex gap-4 md:gap-10 items-center">
+        <li>
+          <Link href="/About" className="text-gray-300 hover:text-white transition-colors text-sm md:text-base">
+            About Us
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/" className="text-gray-300 hover:text-white transition-colors text-sm md:text-base">
+            2025 Board
+          </Link>
+        </li>
+      </ul>
+
+    </nav>
+  );
+};
+
+export default Navbar;
