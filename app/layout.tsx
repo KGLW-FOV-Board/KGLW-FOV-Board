f997dfcb20dbaaa67656f95613daac3efc170dea
Kyle <kjjust7@gmail.com>
2026-06-11 01:36:20 -0700

    Added Google analytics data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import './globals.css'
 import {Geist, Geist_Mono} from "next/font/google"
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +26,7 @@ export default function RootLayout({
         <Navbar></Navbar>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-MPK9CF123D" />
     </html>
   );
 }
